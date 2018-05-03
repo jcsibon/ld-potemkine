3956726c1e92049834580a1b40a62ce3fe15d1d3
Guillaume Bellanger <guillaume.bellanger@gmail.com>
2018-05-03 15:00:45 +0200

    updating for test index.php

--- src/index.php
+++ src/index.php
@@ -8,8 +8,6 @@ require('../vendor/autoload.php');
 
 $app = new Silex\Application();
 
-$app['debug'] = true;
-
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
 	'monolog.logfile' => 'php://stderr',
